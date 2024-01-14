b97742932b41b4fcd5536910c21061303c1f2088
zhilwan <117037336+zhilwan78@users.noreply.github.com>
2024-01-14 10:31:59 +0000

    Add files via upload

---
+++ code.js
@@ -0,0 +1,27 @@
+function downloadCV() {
+    // Replace "path/to/your-cv-file.pdf" with the actual path to your CV file
+    var cvFilePath = "C:\Users\zl1on\OneDrive\Desktop\persone profile";
+    
+    var link = document.createElement("a");
+    link.href = cvFilePath;
+    link.download = "CV.pdf"; // Set the desired name for the downloaded file
+    link.click();
+}
+
+
+
+
+// script.js
+
+function showPhoneNumber() {
+    // Get the element with the phone number
+    var phoneNumberElement = document.getElementById("+964 7511209291");
+  
+    // Toggle the display property to show/hide the phone number
+    if (phoneNumberElement.style.display === "none") {
+      phoneNumberElement.style.display = "block";
+    } else {
+      phoneNumberElement.style.display = "none";
+    }
+  }
+  
